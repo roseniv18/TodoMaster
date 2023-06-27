3397acccfb904da70a953ef1f575aaa7a15c4e37
roseniv18 <87610719+Avitohol1@users.noreply.github.com>
2023-06-27 18:52:36 +0300

    added PUT and DELETE todo functionality

--- server/controllers/todoController.js
+++ server/controllers/todoController.js
@@ -28,7 +28,38 @@ const setTodo = asyncHandler(async (req, res) => {
     res.status(200).json(todo)
 })
 
+// @desc Update goal
+// @route PUT /api/goals/:id
+// @access PRIVATE
+const updateTodo = asyncHandler(async (req, res) => {
+    if (!req.params.id) {
+        res.status(400)
+        throw new Error("No id!")
+    }
+
+    const updatedTodo = await Todo.findByIdAndUpdate(req.params.id, req.body, {
+        new: true,
+    })
+
+    res.status(200).json(updatedTodo)
+})
+
+// @desc Delete goal
+// @route DELETE /api/goals/:id
+// @access PRIVATE
+const deleteTodo = asyncHandler(async (req, res) => {
+    if (!req.params.id) {
+        res.status(400)
+        throw new Error("No id!")
+    }
+
+    await Todo.findByIdAndDelete(req.params.id)
+    res.status(200).send(req.params.id)
+})
+
 module.exports = {
     getTodos,
     setTodo,
+    updateTodo,
+    deleteTodo,
 }
--- server/routes/todoRoutes.js
+++ server/routes/todoRoutes.js
@@ -1,7 +1,13 @@
 const express = require("express")
 const router = express.Router()
-const { getTodos, setTodo } = require("../controllers/todoController")
+const {
+    getTodos,
+    setTodo,
+    updateTodo,
+    deleteTodo,
+} = require("../controllers/todoController")
 
 router.route("/").get(getTodos).post(setTodo)
+router.route("/:id").put(updateTodo).delete(deleteTodo)
 
 module.exports = router
