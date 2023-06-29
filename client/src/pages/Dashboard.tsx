38ad17bf41f5cfecca3a9e287cb42e68ed556307
roseniv18 <87610719+Avitohol1@users.noreply.github.com>
2023-06-29 11:38:37 +0300

    Create, read, delete functionality with user auth

--- client/src/pages/Dashboard.tsx
+++ client/src/pages/Dashboard.tsx
@@ -1,27 +1,97 @@
-import { useEffect } from "react"
+import { ChangeEvent, useEffect, useState } from "react"
 import { useAppDispatch, useAppSelector } from "../redux/store"
 import { useNavigate } from "react-router-dom"
 import { logoutUser, reset } from "../redux/userSlice"
+import { createTodo, deleteTodo, getTodos } from "../redux/todoSlice"
+
+type NewTodo = {
+    text: string
+}
 
 const Dashboard = () => {
+    const [newTodo, setNewTodo] = useState<NewTodo>({
+        text: "",
+    })
+    const { isLoading, isError, isSuccess, todos } = useAppSelector(
+        (store) => store.todos
+    )
     const { user } = useAppSelector((store) => store.user)
     const dispatch = useAppDispatch()
     const navigate = useNavigate()
 
+    const { text } = newTodo
+
+    const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+        const { name, value } = e.target
+        setNewTodo((prev) => {
+            return {
+                ...prev,
+                [name]: value,
+            }
+        })
+    }
+
+    const handleSubmit = (e: ChangeEvent<HTMLFormElement>) => {
+        e.preventDefault()
+        if (!text) {
+            alert("Text field cannot be empty!")
+            return
+        }
+        dispatch(createTodo(newTodo))
+    }
+
     useEffect(() => {
+        if (isError) {
+            console.log("err")
+        }
+
         if (!user._id) {
             navigate("/login")
         }
 
+        dispatch(getTodos())
+
         return () => {
             dispatch(reset())
         }
-    }, [user])
+    }, [user, isError])
+
+    if (isLoading) {
+        return <h2>Loading...</h2>
+    }
 
     return (
         <div>
             <h1>dashboard</h1>
             <button onClick={() => dispatch(logoutUser())}>logout</button>
+            <h2>Add new goal</h2>
+            <form onSubmit={handleSubmit}>
+                <input
+                    type="text"
+                    name="text"
+                    id="text"
+                    value={text}
+                    placeholder="Add new goal..."
+                    onChange={handleChange}
+                />
+                <button type="submit">add</button>
+            </form>
+            <div className="todos">
+                {todos.length > 0 ? (
+                    todos.map((todo) => {
+                        return (
+                            <article key={todo._id}>
+                                <p>{todo.text}</p>
+                                <button onClick={() => dispatch(deleteTodo(todo._id))}>
+                                    X
+                                </button>
+                            </article>
+                        )
+                    })
+                ) : (
+                    <h2>No todos found </h2>
+                )}
+            </div>
         </div>
     )
 }
--- client/src/redux/todoSlice.ts
+++ client/src/redux/todoSlice.ts
@@ -1,9 +1,12 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit"
 import { Todo } from "../types/Todo"
+import axios from "axios"
 
 type TodoState = {
     todos: Todo[]
     isLoading: boolean
+    isSuccess: boolean
+    isError: boolean
     alert: {
         show: boolean
         type: string
@@ -11,9 +14,93 @@ type TodoState = {
     }
 }
 
+// CREATE TODO
+export const createTodo = createAsyncThunk(
+    "todo/create",
+    async (data: { text: string }, thunkAPI) => {
+        try {
+            // @ts-ignore
+            const token: string = thunkAPI.getState().user.user.token
+            const config = {
+                headers: {
+                    Authorization: `Bearer ${token}`,
+                },
+            }
+            const res = await axios.post(`http://localhost:5005/api/todos`, data, config)
+
+            return res.data
+        } catch (error) {
+            let message: string = ""
+            if (typeof error === "string") {
+                message = error
+            } else if (error instanceof Error) {
+                message = error.message
+            }
+            return thunkAPI.rejectWithValue(message)
+        }
+    }
+)
+
+// GET ALL TODOS
+export const getTodos = createAsyncThunk("todo/getAll", async (_, thunkAPI) => {
+    try {
+        // @ts-ignore
+        const token: string = thunkAPI.getState().user.user.token
+        const config = {
+            headers: {
+                Authorization: `Bearer ${token}`,
+            },
+        }
+        const res = await axios.get(`http://localhost:5005/api/todos`, config)
+        return res.data
+    } catch (error) {
+        let message: string = ""
+        if (typeof error === "string") {
+            message = error
+        } else if (error instanceof Error) {
+            message = error.message
+        }
+        return thunkAPI.rejectWithValue(message)
+    }
+})
+
+// DELETE TODO
+export const deleteTodo = createAsyncThunk(
+    "todo/delete",
+    async (id: string, thunkAPI) => {
+        try {
+            // @ts-ignore
+            const token: string = thunkAPI.getState().user.user.token
+            const config = {
+                headers: {
+                    Authorization: `Bearer ${token}`,
+                },
+            }
+            const res = await axios.delete(
+                `http://localhost:5005/api/todos/${id}`,
+                config
+            )
+            return res.data
+        } catch (error) {
+            let message: string = ""
+            if (typeof error === "string") {
+                message = error
+            } else if (error instanceof Error) {
+                message = error.message
+            }
+            return thunkAPI.rejectWithValue(message)
+        }
+    }
+)
+
+// UPDATE TODO
+// export const updateTodo = createAsyncThunk("todo/update", async(_, thunkAPI))
+
 const initialState: TodoState = {
     todos: [],
     isLoading: false,
+    isSuccess: false,
+    isError: false,
     alert: {
         show: false,
         type: "",
@@ -25,6 +112,80 @@ const todoSlice = createSlice({
     name: "todos",
     initialState,
     reducers: {},
+    extraReducers: (builder) => {
+        // CREATE TODO
+        builder.addCase(createTodo.pending, (state) => {
+            state.isLoading = true
+        })
+        builder.addCase(createTodo.fulfilled, (state, action) => {
+            state.isLoading = false
+            state.isSuccess = true
+            state.todos = [...state.todos, action.payload]
+            state.alert = {
+                show: true,
+                type: "success",
+                msg: `Created new goal!`,
+            }
+        })
+        builder.addCase(createTodo.rejected, (state, action) => {
+            state.isLoading = false
+            state.isError = true
+            if (typeof action.payload === "string") {
+                state.alert = {
+                    show: true,
+                    type: "error",
+                    msg: `Error: ${action.payload}`,
+                }
+            }
+        })
+
+        // GET ALL TODOS
+        builder.addCase(getTodos.pending, (state) => {
+            state.isLoading = true
+        })
+        builder.addCase(getTodos.fulfilled, (state, action) => {
+            state.isLoading = false
+            state.isSuccess = true
+            state.todos = action.payload
+        })
+        builder.addCase(getTodos.rejected, (state, action) => {
+            state.isLoading = false
+            state.isError = true
+            if (typeof action.payload === "string") {
+                state.alert = {
+                    show: true,
+                    type: "error",
+                    msg: `Error: ${action.payload}`,
+                }
+            }
+        })
+
+        // DELETE TODO
+        builder.addCase(deleteTodo.pending, (state) => {
+            state.isLoading = true
+        })
+        builder.addCase(deleteTodo.fulfilled, (state, action) => {
+            state.isLoading = false
+            state.isSuccess = true
+            state.todos = state.todos.filter((todo) => todo._id !== action.payload)
+            state.alert = {
+                show: true,
+                type: "success",
+                msg: `Deleted goal ${action.payload}`,
+            }
+        })
+        builder.addCase(deleteTodo.rejected, (state, action) => {
+            state.isLoading = false
+            state.isError = true
+            if (typeof action.payload === "string") {
+                state.alert = {
+                    show: true,
+                    type: "error",
+                    msg: `Error: ${action.payload}`,
+                }
+            }
+        })
+    },
 })
 
 export default todoSlice.reducer
