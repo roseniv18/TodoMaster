5c50d37b9304ecd70751cf1f38367638fd1d9ed6
roseniv18 <87610719+Avitohol1@users.noreply.github.com>
2023-07-05 20:28:36 +0300

    preparing for deployment

--- client/src/redux/todoThunks/deleteTodoThunk.ts
+++ client/src/redux/todoThunks/deleteTodoThunk.ts
@@ -1,4 +1,3 @@
-import axios from "axios"
 import axiosInstance from "../../helpers/axiosInstance"
 
 export const deleteTodoThunk = async (id: string, thunkAPI: any) => {
--- client/src/redux/todoThunks/updateTodoThunk.ts
+++ client/src/redux/todoThunks/updateTodoThunk.ts
@@ -1,4 +1,3 @@
-import axios from "axios"
 import axiosInstance from "../../helpers/axiosInstance"
 
 export const updateTodoThunk = async (
