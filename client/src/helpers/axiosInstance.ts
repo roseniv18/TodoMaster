f474953f483311fd54a63e1d3720b92b079e370c
roseniv18 <87610719+Avitohol1@users.noreply.github.com>
2023-07-05 19:57:28 +0300

    preparing for deployment

--- client/src/helpers/axiosInstance.ts
+++ client/src/helpers/axiosInstance.ts
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const axiosInstance = axios.create({
-    baseURL: "http://todo-master.onrender.com/api",
+    baseURL: "https://todo-master.onrender.com/api",
 })
 
 export default axiosInstance
